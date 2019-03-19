357592f6ca2392cf301f9dc1053481570fcb72e8
DeyuHeng <1119242459@qq.com>
2019-03-20 09:00:11 +1100

    :bug: Fix 'ServiceContestHolder' config

--- dynabiz-spring-web/src/main/java/io/dynabiz/web/context/config/ServiceContextConfig.java
+++ dynabiz-spring-web/src/main/java/io/dynabiz/web/context/config/ServiceContextConfig.java
@@ -2,19 +2,18 @@ package io.dynabiz.web.context.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.dynabiz.web.context.ServiceContextLoaderListener;
-import io.dynabiz.web.context.ServiceContextSerializer;
-import io.dynabiz.web.context.StringServiceContextSerializer;
+import io.dynabiz.web.context.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.Configuration;
 
 public class ServiceContextConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    public ServiceContextSerializer serviceContextSerializer(ObjectMapper mapper){
+    public ServiceContextSerializer serviceContextSerializer(){
+        ObjectMapper mapper = new ObjectMapper();
         return new StringServiceContextSerializer(mapper);
     }
 
@@ -24,4 +23,9 @@ public class ServiceContextConfig {
         return new ServletListenerRegistrationBean<>(new ServiceContextLoaderListener());
     }
 
+    @Bean
+    public ServiceContextHolder serviceContextHolder(ServiceContextSerializer serializer,
+                                                     ServiceContextStorage storage){
+        return new ServiceContextHolder(serializer, storage);
+    }
 }
