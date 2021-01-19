9cde65a42fcb230365453f7e80b764c60cfd7678
Fcant <fcscanf@outlook.com>
2021-01-19 09:46:43 +0800

    :bug: Fixed an issue where the uniform return result failed to intercept

--- dynabiz-spring-web/src/main/java/org/dynabiz/web/response/config/ResponseBodyWrapConfig.java
+++ dynabiz-spring-web/src/main/java/org/dynabiz/web/response/config/ResponseBodyWrapConfig.java
@@ -1,32 +1,22 @@
 package org.dynabiz.web.response.config;
 
-import org.dynabiz.web.response.GeneralResponse;
 import org.dynabiz.web.response.wrapper.ResponseBodyWrapFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
 
 @Configuration
-public class ResponseBodyWrapConfig implements ResponseBodyAdvice {
+public class ResponseBodyWrapConfig extends WebMvcConfigurationSupport {
 
-    public boolean supports(MethodParameter methodParameter, Class aClass) {
-        return true;
+    @Override
+    @Bean
+    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+        return super.requestMappingHandlerAdapter();
     }
 
-    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
-        if(o instanceof ResponseEntity){
-            return o;
-        }
-        else {
-            return new GeneralResponse(o);
-        }
+    @Bean
+    public ResponseBodyWrapFactoryBean getResponseBodyWrap() {
+        return new ResponseBodyWrapFactoryBean();
     }
 }
